5543d6dda9251772f96991f0da2272eb4ec60f13
lokhno <nlokhno@gmail.com>
2021-08-12 01:35:26 +0300

    Корректировка компонента Button и стилей TopPanel

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 import "./Button.scss";
 
-function Button({ children, className, handleClick }) {
+function Button({ children, className, handleClick, name }) {
     return (
         <div onClick={handleClick} className={`button ${className}`}>
-            {children}
+            <div className="button__title">{name}</div>
+            <div className="button__icon">{children}</div>
         </div>
     );
 }
--- src/components/ControlMenu/index.jsx
+++ src/components/ControlMenu/index.jsx
@@ -7,10 +7,18 @@ import "./ControlMenu.scss";
 function ControlMenu() {
     return (
         <div className="control-menu">
-            <Button className="control-menu__button">Добавить</Button>
-            <Button className="control-menu__button">Редактировать</Button>
-            <Button className="control-menu__button">Удалить</Button>
-            <Button className="control-menu__button">Перемешать</Button>
+            <Button className="control-menu__button" name="Добавить">
+                <i class="fas fa-plus"></i>
+            </Button>
+            <Button className="control-menu__button" name="Редактировать">
+                <i class="far fa-edit"></i>
+            </Button>
+            <Button className="control-menu__button" name="Удалить">
+                <i class="far fa-trash-alt"></i>
+            </Button>
+            <Button className="control-menu__button" name="Перемешать">
+                <i class="fas fa-recycle"></i>
+            </Button>
         </div>
     );
 }
