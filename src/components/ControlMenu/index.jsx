b8969f315c28a6f321c78e44920754938c828737
lokhno <nlokhno@gmail.com>
2021-08-17 23:47:42 +0300

    Добавление кнопки учить слова

--- src/components/ControlMenu/index.jsx
+++ src/components/ControlMenu/index.jsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import classnames from "classnames";
 
-import { Button } from "../";
+import { Button, Form } from "../";
 
 import "./ControlMenu.scss";
 
-function ControlMenu({className}) {
+function ControlMenu({ className }) {
+    const [overlayHidden, setOverlayHidden] = useState(false);
     return (
-        <div className={`control-menu ${className}`}>
-            <Button className="control-menu__button" name="Добавить">
-                <i className="fas fa-plus"></i>
-            </Button>
-            <Button className="control-menu__button" name="Редактировать">
-                <i className="far fa-edit"></i>
-            </Button>
-            <Button className="control-menu__button" name="Удалить">
-                <i className="far fa-trash-alt"></i>
-            </Button>
-            <Button className="control-menu__button" name="Перемешать">
-                <i className="fas fa-recycle"></i>
-            </Button>
+        <div>
+            <Form overlayHidden={overlayHidden} setOverlayHidden={setOverlayHidden} />
+
+            <div className={`control-menu ${className}`}>
+                <Button className="control-menu__button" name="Добавить" handleClick={() => setOverlayHidden(!overlayHidden)}>
+                    <i className="fas fa-plus"></i>
+                </Button>
+                <Button className="control-menu__button" name="Редактировать">
+                    <i className="far fa-edit"></i>
+                </Button>
+                <Button className="control-menu__button" name="Удалить">
+                    <i className="far fa-trash-alt"></i>
+                </Button>
+                <Button className="control-menu__button" name="Перемешать">
+                    <i className="fas fa-recycle"></i>
+                </Button>
+                <Button className="control-menu__button" name="Учить">
+                    <i class="fas fa-graduation-cap"></i>
+                </Button>
+            </div>
         </div>
     );
 }
